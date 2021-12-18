f251a5ed0811073ff5e30fbe4d0012c24fa1ba09
Matthew Castrigno <matthewcastrigno@u.boisestate.edu>
2021-12-18 14:44:57 -0700

    prints board and puts in tokens

---
+++ ConnectFour/GameBoard.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour
+{
+    class GameBoard
+    {
+        // members initalized game board
+        // c# will init with zeros.
+        int[,] board = new int[7, 7];
+
+
+        public GameBoard()
+        {
+            this.board = new int[7, 7];
+
+        }
+
+        public void DropToken(ValueTuple<int, int> token)
+        {
+            Console.WriteLine(token.Item1 + " " + token.Item2);
+
+            //item1 is your player number, only used on assignment
+            //item2 is your slot    
+            // i is the row and item2 is the column
+            // iterate over the row 
+            for (int i = 0; i < board.GetLength(1); i++)
+            {
+
+                if (i == 0 && board[i, token.Item2] != 0) //full column
+                {
+                    Console.WriteLine("Error - That slot is full. Please pick another slot");
+                    break;
+                }
+                if ((i == board.GetLength(1) - 1) && board[i, token.Item2] == 0) // bottom
+                {
+                    board[i, token.Item2] = token.Item1;
+                    break;
+                }
+                if (board[i, token.Item2] != 0) // mid case
+                {
+                    board[i - 1, token.Item2] = token.Item1;
+                    break;
+                }
+
+            }
+
+
+        }
+        override public string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(" 1 2 3 4 5 6 7");
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    sb.Append(" " + board[i, j]);
+                }
+                sb.AppendLine("");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ConnectFour/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConnectFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConnectFour! Let's Play!");
+            Console.WriteLine();
+            GameBoard board = new();
+            Console.WriteLine(board.ToString());
+
+
+
+            Console.WriteLine("Pick a Slot 1 - 7 to drop your token in.");
+            //(int, int) tokenToDrop = (1,1);
+            ValueTuple<int, int> tokenToDrop = new();
+            tokenToDrop.Item1 = 1;
+            tokenToDrop.Item2 = 2;
+            board.DropToken(tokenToDrop);
+            Console.WriteLine(board.ToString());
+            tokenToDrop.Item1 = 4;
+            tokenToDrop.Item2 = 6;
+            board.DropToken(tokenToDrop);
+            Console.WriteLine(board.ToString());
+            tokenToDrop.Item1 = 4;
+            tokenToDrop.Item2 = 6;
+            board.DropToken(tokenToDrop);
+            Console.WriteLine(board.ToString());
+            tokenToDrop.Item1 = 4;
+            tokenToDrop.Item2 = 6;
+            board.DropToken(tokenToDrop);
+            Console.WriteLine(board.ToString());
+            tokenToDrop.Item1 = 4;
+            tokenToDrop.Item2 = 6;
+            board.DropToken(tokenToDrop);
+            Console.WriteLine(board.ToString());
+            tokenToDrop.Item1 = 4;
+            tokenToDrop.Item2 = 6;
+            board.DropToken(tokenToDrop);
+            Console.WriteLine(board.ToString());
+            tokenToDrop.Item1 = 4;
+            tokenToDrop.Item2 = 6;
+            board.DropToken(tokenToDrop);
+            Console.WriteLine(board.ToString());
+            tokenToDrop.Item1 = 4;
+            tokenToDrop.Item2 = 6;
+            board.DropToken(tokenToDrop);
+            Console.WriteLine(board.ToString());
+            tokenToDrop.Item1 = 4;
+            tokenToDrop.Item2 = 6;
+            board.DropToken(tokenToDrop);
+            Console.WriteLine(board.ToString());
+        }
+    }
+}
